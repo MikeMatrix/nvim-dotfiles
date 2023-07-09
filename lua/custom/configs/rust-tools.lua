aefa93ded45df553d5ddee8be1e9305df4ec3fd2
Michael Braun <m.braun92@gmail.com>
2023-07-09 09:28:42 +0200

    fix: missing configs for rust

--- lua/custom/configs/rust-tools.lua
+++ lua/custom/configs/rust-tools.lua
@@ -1,10 +1,20 @@
 local on_attach = require("plugins.configs.lspconfig").on_attach
 local capabilities = require("plugins.configs.lspconfig").capabilities
+local util = require "lspconfig/util"
 
 local options = {
   server = {
     on_attach = on_attach,
     capabilities = capabilities,
+    filetypes = { "rust" },
+    root_dir = util.root_pattern "Cargo.toml",
+    settings = {
+      ["rust-analyzer"] = {
+        cargo = {
+          allFeatures = true,
+        },
+      },
+    },
   },
 }
 
