d1d172731616ab972869ee4695dec8696f1861ee
Michael Braun <m.braun92@gmail.com>
2023-07-20 11:36:23 +0200

    add wsl support for clipboard

--- lua/custom/init.lua
+++ lua/custom/init.lua
@@ -1,6 +1,25 @@
 vim.api.nvim_exec("language en_US", true)
 
+-- GUI Config
 vim.o.guifont = "FiraCode Nerd Font:h11"
+
+-- WSL Specific
+local in_wsl = os.getenv "WSL_DISTRO_NAME" ~= nil
+
+if in_wsl then
+  vim.g.clipboard = {
+    name = "WslClipboard",
+    copy = {
+      ["+"] = "clip.exe",
+      ["*"] = "clip.exe",
+    },
+    paste = {
+      ["+"] = 'powershell.exe -c [Console]::Out.Write($(Get-Clipboard -Raw).tostring().replace("`r", ""))',
+      ["*"] = 'powershell.exe -c [Console]::Out.Write($(Get-Clipboard -Raw).tostring().replace("`r", ""))',
+    },
+    cache_enabled = false,
+  }
+end
 
 -- Indenting
 vim.opt.expandtab = true
