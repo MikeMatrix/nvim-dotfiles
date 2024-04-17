63716893ca4f09a2fa677180aee480b63e991bbb
Michael Braun <m.braun92@gmail.com>
2024-04-17 08:59:42 +0200

    feat: add windows specific config to use win32yank

--- lua/options.lua
+++ lua/options.lua
@@ -1,7 +1,10 @@
 require "nvchad.options"
 
+local uv = vim.uv or vim.loop
+
 -- WSL Specific
 local in_wsl = os.getenv "WSL_DISTRO_NAME" ~= nil
+local in_windows = uv.os_uname().sysname == "Windows_NT"
 
 if in_wsl then
   vim.g.clipboard = {
@@ -11,11 +14,26 @@ if in_wsl then
       ["*"] = "win32yank.exe -i --crlf",
     },
     paste = {
-      ["+"] = 'win32yank.exe -o --lf',
-      ["*"] = 'win32yank.exe -o --lf',
+      ["+"] = "win32yank.exe -o --lf",
+      ["*"] = "win32yank.exe -o --lf",
     },
     cache_enabled = false,
   }
+else
+  if in_windows then
+    vim.g.clipboard = {
+      name = "win32yank-windows",
+      copy = {
+        ["+"] = "win32yank.exe -i --crlf",
+        ["*"] = "win32yank.exe -i --crlf",
+      },
+      paste = {
+        ["+"] = "win32yank.exe -o --crlf",
+        ["*"] = "win32yank.exe -o --crlf",
+      },
+      cache_enabled = false,
+    }
+  end
 end
 
 -- Indenting
@@ -30,4 +48,3 @@ vim.opt.foldmethod = "expr"
 vim.opt.foldexpr = "nvim_treesitter#foldexpr()"
 vim.opt.foldenable = true
 vim.opt.foldlevel = 99
-
